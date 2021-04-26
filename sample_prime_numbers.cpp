02ee0572a253f74e0dc009514ae002f17addec39
Svetlana531 <60916371+Svetlana531@users.noreply.github.com>
2021-04-26 12:14:54 +0300

    Add files via upload

---
+++ sample_prime_numbers.cpp
@@ -0,0 +1,139 @@
+﻿
+
+#include <iomanip>
+
+#define PathForFile "C:\\out.txt"
+// #define USE_SET // Использовать класс TSet,
+                // закоментировать, чтобы использовать битовое поле
+
+#ifndef USE_SET // Использовать класс TBitField
+
+#include "tbitfield.h"
+
+int main()
+{
+  int n, m, k, count;
+
+  setlocale(LC_ALL, "Russian");
+  cout << "Тестирование программ поддержки битового поля" << endl;
+  cout << "             Решето Эратосфена" << endl;
+  cout << "Введите верхнюю границу целых значений - ";
+  cin  >> n;
+  TBitField s(n + 1);
+  // заполнение множества
+  for (m = 2; m <= n; m++)
+    s.SetBit(m);
+
+  
+
+  // проверка до sqrt(n) и удаление кратных
+  for (m = 2; m * m <= n; m++)
+    // если m в s, удаление кратных
+    if (s.GetBit(m))
+      for (k = 2 * m; k <= n; k += m)
+        if (s.GetBit(k))
+          s.ClrBit(k);
+  // оставшиеся в s элементы - простые числа
+  cout << endl << "Печать множества некратных чисел" << endl << s << endl;
+  cout << endl << "Печать простых чисел" << endl;
+  count = 0;
+  k = 1;
+  for (m = 2; m <= n; m++)
+    if (s.GetBit(m))
+    {
+      count++;
+      cout << setw(3) << m << " ";
+      if (k++ % 10 == 0)
+        cout << endl;
+    }
+  cout << endl;
+  cout << "В первых " << n << " числах " << count << " простых" << endl;
+
+  s.InFile(PathForFile);
+
+  TBitField s_2(n + 1);
+  s_2.FromFile(PathForFile);
+  std::cout << s_2;
+}
+#else
+
+#include "tset.h"
+
+int main()
+{
+  int n, m, k, count;
+
+  setlocale(LC_ALL, "Russian");
+  cout << "Тестирование программ поддержки множества" << endl;
+  cout << "              Решето Эратосфена" << endl;
+  cout << "Введите верхнюю границу целых значений - ";
+  cin  >> n;
+  TSet s(n + 1);
+  // заполнение множества
+  //for (m = 2; m <= n; m++)
+   
+  
+  s.InsElem(2);
+  s.InsElem(5);
+  s.InsElem(7);
+  s.InsElem(10);
+  s.InsElem(11);
+  s.InsElem(12);
+  s.InsElem(17);
+  s.InsElem(19);
+
+
+  // проверка до sqrt(n) и удаление кратных
+
+  std::cout << "\n";
+  std::cout << s;
+  std::cout << "\n";
+  s.InFile(PathForFile);
+  //s.getElements(5);
+  std::cout << "\n";
+
+  for (m = 2; m * m <= n; m++)
+    // если м в s, удаление кратных
+    if (s.IsMember(m))
+      for (k = 2 * m; k <= n; k += m)
+       if (s.IsMember(k))
+         s.DelElem(k);
+  // оставшиеся в s элементы - простые числа
+  cout << endl << "Печать множества некратных чисел" << endl << s << endl;
+  cout << endl << "Печать простых чисел" << endl;
+  count = 0;
+  k = 1;
+  for (m = 2; m <= n; m++)
+    if (s.IsMember(m))
+    {
+      count++;
+      cout << setw(3) << m << " ";
+      if (k++ % 10 == 0)
+        cout << endl;
+    }
+  cout << endl;
+  cout << "В первых " << n << " числах " << count << " простых" << endl;
+
+
+  TSet s_2(n + 1);
+  s_2.FromFile(PathForFile);
+
+
+  std::cout << "\n";
+  std::cout << s_2;
+  s_2.ChangeElements(2, 18); 
+  std::cout << "\n";
+  std::cout << "\n";
+  std::cout << s_2;
+  std::cout << "\n";
+  s_2.getElements(5);
+
+  TSet s_3(n + 1);
+  s_3 = s_2.getElements(5);
+  std::cout << s_3;
+
+ // s_2.InFile(PathForFile);
+ // s_2.getElements(2);
+}
+
+#endif
